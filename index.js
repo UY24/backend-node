b0a26f91b849a9fe39f57c7a341aef9ea9208c1a
UY24 <ujjwal.uy03@gmail.com>
2023-05-04 23:25:32 +0530

    c

--- index.js
+++ index.js
@@ -18,15 +18,15 @@ app.use(
     
   })
 );
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-//   );
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  // res.header(
+  //   "Access-Control-Allow-Headers",
+  //   "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  // );
+  next();
+});
 
 // mongoose.set("useNewUrlParser", true);
 // mongoose.set("useFindAndModify", false);
